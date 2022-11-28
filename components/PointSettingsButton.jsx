690332e8da5e293467fcb48c173578bc9499e76f
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-28 22:07:15 +0100

    Finis styling button

--- components/PointSettingsButton.jsx
+++ components/PointSettingsButton.jsx
@@ -1,19 +1,18 @@
 import FingerPrint from "./FingerPrint";
 
-const PointSettingsButton = ({ coordX, coordY, name }) => {
-  console.log(coordX, coordY);
-  return (
-    <button
-      className={"absolute bg-black text-white rounded-full opacity-50"}
-      style={{
-        top: `${coordY}%`,
-        left: `${coordX}%`,
-      }}
-    >
-      <FingerPrint />
-      <span className="sr-only">{name}</span>
-    </button>
-  );
-};
+const PointSettingsButton = ({ coordX, coordY, name }) => (
+  <button
+    className={
+      "absolute box-border bg-black text-white rounded-full opacity-50 p-1 ring ring-offset-2 ring-black hover:opacity-20"
+    }
+    style={{
+      top: `${coordY}%`,
+      left: `${coordX}%`,
+    }}
+  >
+    <FingerPrint />
+    <span className="sr-only">{name}</span>
+  </button>
+);
 
 export default PointSettingsButton;
