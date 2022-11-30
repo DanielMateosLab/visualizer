b6b3bbab7fd4fc72c0bfb6d4af5845748f2a1e39
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-30 07:46:39 +0100

    Add view state

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import PointSettingsButton from "../components/PointSettingsButton";
 import { db, getPoints } from "../firebase/db";
 
 export default function Home(props) {
+  const [settings, setSettings] = useState({});
+  const [materialsMenuPoint, setMaterialsMenuPoint] = useState(null);
+
   return (
     <div className="h-screen w-screen relative flex overflow-hidden">
       <Background />
