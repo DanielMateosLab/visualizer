a691b9b32798b35787ca8e2eb500edc90499431e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-30 10:16:38 +0100

    Create useMaterialsForPoint Hook with a mock implementation

---
+++ hooks/useMaterialsForPoint.js
@@ -0,0 +1,24 @@
+export const useMaterialsForPoint = (pointId) => {
+  return {
+    nXON8cI5bRb6OsC93Xz0: {
+      materialPreview:
+        "https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/materials_1563188186943_preview.jpeg?alt=media&token=f76c0914-2bfc-4055-9bc7-92a5a7b412bd",
+      name: "Roble Tierra",
+      points: ["Ks5CthbPwAvd2TNxzHEl"],
+      layers: {
+        Ks5CthbPwAvd2TNxzHEl:
+          "https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/materials_1563188186943_KTQyQxRXM6se2nREr5zZ-1591364026128-Ks5CthbPwAvd2TNxzHEl.png?alt=media&token=3a6562d6-916f-4fcf-a771-b28e450d2add",
+      },
+    },
+    xY2AvcJKYi98TgBayVxy: {
+      points: ["Ks5CthbPwAvd2TNxzHEl"],
+      materialPreview:
+        "https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/materials_1578585661748_preview.jpeg?alt=media&token=69c30089-df31-444d-b6b4-ebe3bb19a7bc",
+      name: "Etimoe Ice",
+      layers: {
+        Ks5CthbPwAvd2TNxzHEl:
+          "https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/materials_1578585661748_KTQyQxRXM6se2nREr5zZ-1591364026128-Ks5CthbPwAvd2TNxzHEl.png?alt=media&token=062b38ee-990f-4d61-897e-444359f3cd75",
+      },
+    },
+  };
+};
