a775288b94638c5bb1674d1a7d75bcb3f26e85c1
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-12-01 14:34:52 +0100

    Translate buttons half width and height

--- components/PointSettingsButton.jsx
+++ components/PointSettingsButton.jsx
@@ -1,29 +1,29 @@
-import FingerPrint from "./FingerPrint";
+import FingerPrint from './FingerPrint'
 
 const PointSettingsButton = ({
   coordX,
   coordY,
   name,
   onClick,
-  selectingMaterial,
+  selectingMaterial
 }) => {
-  const settingsButtonVisibilityClass = selectingMaterial ? "invisible" : "";
+  const settingsButtonVisibilityClass = selectingMaterial ? 'invisible' : ''
 
   return (
     <button
       className={`absolute box-border bg-black text-white rounded-full
        opacity-50 p-1 ring ring-offset-2 ring-black hover:opacity-20 
-       z-20 ${settingsButtonVisibilityClass}`}
+       z-20 -translate-x-1/2 -translate-y-1/2 ${settingsButtonVisibilityClass}`}
       style={{
         top: `${coordY}%`,
-        left: `${coordX}%`,
+        left: `${coordX}%`
       }}
       onClick={onClick}
     >
       <FingerPrint />
-      <span className="sr-only">{name}</span>
+      <span className='sr-only'>{name}</span>
     </button>
-  );
-};
+  )
+}
 
-export default PointSettingsButton;
+export default PointSettingsButton
